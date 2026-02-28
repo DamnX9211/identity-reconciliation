a4a7d3d3ef5bbb9540650c6c6fcf79ac850bb942
DamnX9211 <rohitkuumar1995@gmail.com>
2026-02-28 14:29:46 +0530

    Initial project setup with Express and TypeScript

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Bitespace Identity Reconciliation API is running!");
+});
+
+export default app;
